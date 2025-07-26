886dd6e818e74f2d62d021e6b6ef593fd820c794
João Rafael Quintiliano de Souza <73571243+JoaoRafaelSouza@users.noreply.github.com>
2025-07-26 17:18:52 -0300

    Ajustes aplicados ao CRM:
    * Foi feito um ajuste para buscar os dados do Portal da Transparência, tudo está vindo direto por um Json.

--- backend/app/services/scraper_portal.py
+++ backend/app/services/scraper_portal.py
@@ -105,25 +105,28 @@ class PortalTransparenciaScraper:
                 time.sleep(2)
                 imagem_tela(self.driver, "pag")
             
-            todos_botoes = self.driver.find_elements(By.CSS_SELECTOR, '.br-button.secondary.mt-3')
-            resultado_beneficios = [eliminado for eliminado in todos_botoes if 'block' not in eliminado.get_attribute('class')][:10]
-            time.sleep(2)
-            imagem_tela(self.driver, "pag")
-
-            logger.info(f"Encontrados {len(resultado_beneficios)} resultados")
-            for idx, linha in enumerate(resultado_beneficios):
+            for idx in range(10):
+                todos_botoes = self.driver.find_elements(By.CSS_SELECTOR, '.br-button.secondary.mt-3')
+                resultado_beneficios = [el for el in todos_botoes if 'block' not in el.get_attribute('class')]
+                
+                if idx >= len(resultado_beneficios):
+                    break
+                
+                linha = resultado_beneficios[idx]
                 linha.click()
                 time.sleep(10)
                 imagem_tela(self.driver, "pag")
-
+                
                 screenshot_b64 = self.driver.get_screenshot_as_base64()
                 beneficios = self._coletar_beneficios()
-
+                imagem_tela(self.driver, "pag")
+                
                 resultado_final["resultado"].append({
                     "indice": idx + 1,
                     "beneficios": beneficios,
                     "screenshot_base64": screenshot_b64
                 })
+
             return resultado_final
 
         except TimeoutException:
@@ -143,18 +146,43 @@ class PortalTransparenciaScraper:
             titulo = self.driver.find_element(By.CSS_SELECTOR, '.py-0.px-0.mx-0.mt-0.mb-2.mb-md-0.pb-0.text-left').text.strip()
             time.sleep(2)
 
-            # Expande os detalhes do auxílio
-            self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="accordion1"]/div[1]/button/span[1]'))).click()
+            icone = self.driver.find_element(By.CSS_SELECTOR, 'div.item i.fas')
             time.sleep(2)
+            classe = icone.get_attribute("class")
+            # Verifica se a classe está igual e expande os detalhes do auxílio
+            if "fa-angle-up" in classe:
+                self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="accordion1"]/div[1]/button'))).click()
+                time.sleep(2)
+                imagem_tela(self.driver, "pag")
+            
+            # Aqui ele clica novamente no aceitar cookies na tela Pessoa Física
+            self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="accept-all-btn"]'))).click()
             imagem_tela(self.driver, "pag")
-
+            
+            btnPag = self.driver.find_element(By.ID, 'btnPaginacaoCompleta')
+            time.sleep(2)
+            classe = btnPag.get_attribute("class")
+            if "br-button" in classe:
+                self.wait.until(EC.element_to_be_clickable((By.ID, 'btnPaginacaoCompleta'))).click()
+                time.sleep(2)
+                imagem_tela(self.driver, "pag")
+                self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="tabelaDetalheValoresSacados_length"]/label/select/option[3]'))).click()
+                time.sleep(2)
+                imagem_tela(self.driver, "pag")
+                
+            tabela = self.driver.find_element(By.XPATH,'//*[@id="tabelaDetalheValoresSacados"]')
+            time.sleep(2)
+            
             # Coleta os títulos das colunas
-            cabecalhos = [th.text.strip() for th in self.driver.find_elements(By.CSS_SELECTOR, 'thead tr th')]
+            cabecalhos = [th.text.strip() for th in tabela.find_elements(By.CSS_SELECTOR, 'thead tr th')]
             time.sleep(2)
-
+            
+            # Aqui pegamos todas as linhas do tbody
+            linhas = tabela.find_elements(By.CSS_SELECTOR, 'tbody tr')
+            time.sleep(2)
+            
+            # Aqui temos um arrray que servirá de lista no final
             beneficio = []
-            linhas = self.driver.find_elements(By.CSS_SELECTOR, 'tbody tr')
-            time.sleep(2)
 
             for linha in linhas:
                 try:
